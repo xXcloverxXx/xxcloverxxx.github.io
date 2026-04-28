06403d2699ca7c676166cda480d890770bde3432
xXcloverxXx <xclover.sp@gmail.com>
2026-04-28 08:32:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -104,20 +104,20 @@ async function getSpecialties() {
               <div class="tab-pane fade show active" id="nav-home-${UID}" role="tabpanel" aria-labelledby="nav-home-tab-${UID}" tabindex="0">
                 <p>Количество бюджетных мест: <span class="bold">${item.oBudget}</span></p>
                 <p>Количество контрактных мест:  <span class="bold">${item.oContract}</span></p>
-                <p>Стоимость обучения:  <span class="bold">${item.oYear}</span></p>
-                <p>Срок обучения:  <span class="bold">${item.oPrice}</span></p>
+                <p>Стоимость обучения:  <span class="bold">${item.oPrice}</span></p>
+                <p>Срок обучения:  <span class="bold">${item.oYear}</span></p>
               </div>
               <div class="tab-pane fade" id="nav-profile-${UID}" role="tabpanel" aria-labelledby="nav-profile-tab-${UID}" tabindex="0">
                 <p>Количество бюджетных мест: <span class="bold">${item.ozBudget}</span></p>
                 <p>Количество контрактных мест:  <span class="bold">${item.ozContract}</span></p>
-                <p>Стоимость обучения:  <span class="bold">${item.ozYear}</span></p>
-                <p>Срок обучения:  <span class="bold">${item.ozPrice}</span></p>
+                <p>Стоимость обучения:  <span class="bold">${item.ozPrice}</span></p>
+                <p>Срок обучения:  <span class="bold">${item.ozYear}</span></p>
               </div>
               <div class="tab-pane fade" id="nav-contact-${UID}" role="tabpanel" aria-labelledby="nav-contact-tab-${UID}" tabindex="0">
                 <p>Количество бюджетных мест: <span class="bold">${item.zBudget}</span></p>
                 <p>Количество контрактных мест:  <span class="bold">${item.zContract}</span></p>
-                <p>Стоимость обучения:  <span class="bold">${item.zYear}</span></p>
-                <p>Срок обучения:  <span class="bold">${item.zPrice}</span></p>
+                <p>Стоимость обучения:  <span class="bold">${item.zPrice}</span></p>
+                <p>Срок обучения:  <span class="bold">${item.zYear}</span></p>
               </div>
               <div class="tab-pane fade" id="nav-vi-${UID}" role="tabpanel" aria-labelledby="nav-vi-tab-${UID}" tabindex="0">
                 ${kAndViText}
